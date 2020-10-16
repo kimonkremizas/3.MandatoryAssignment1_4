af93ac214841dbe4078b1056ebf22d1c41eb686e
Kimon Kremyzas <kimo0016@edu.easj.dk>
2020-10-16 16:55:08 +0200

    Add project files.

---
+++ BookTCPServer/BookList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookLibrary;
+
+namespace BookTCPServer
+{
+    public class BookList
+    {
+        public static List<Book> bookList = new List<Book>()
+        {
+            new Book("Jitterbug Perfume","Tom Robbins",342,"9780553348989"),
+            new Book("The Shining","Stephen King",450,"9780385121675"),
+            new Book("The Haunting of Hill House","Shirley Jackson",288,"9780143122357")
+        };
+    }
+}
